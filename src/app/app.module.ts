e03a3c7b4c7a3f69a39ff02f469c37a9b0781931
JAVIER ESCRIBANO GARCIA <jeg11@clL27-18>
2017-01-27 20:19:18 +0100

    taller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {SwapiService} from "./core/swapi.service";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    SwapiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
